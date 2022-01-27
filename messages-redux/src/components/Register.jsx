44148105897c55f8d83ad1c7569cec240629a027
Tom-chack <tom.controlcenter@gmail.com>
2022-01-27 22:25:18 +0400

    Adding redux to a new app.

---
+++ messages-redux/src/components/Register.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {useForm} from 'react-hook-form'
+
+function Register() {
+
+  const { register, handleSubmit, formState, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
+  
+  console.log(errors);
+  console.log(formState);
+  
+  return (
+    <form onSubmit={ handleSubmit( onSubmit ) }>
+      <div className="form-group">
+        <input type="text" {...register("username", {required: true, maxLength: 100})} placeholder="Username" className="form-control"/>
+      </div>
+      <div className="form-group">
+        <input type="text" {...register("email", {required: true, pattern: /^\S+@\S+$/i})} placeholder="Email" className="form-control"/>
+      </div>
+      <div className="form-group">
+        <input type="password" {...register("password", {required: true, min: 3})} placeholder="Password" className="form-control"/>
+      </div>
+      <div className="form-group form-bottom">
+        <input type="submit" name="Register" value="Register" className="btn btn-primary"/>
+      </div>
+    </form>
+  );
+}
+
+export default Register;
