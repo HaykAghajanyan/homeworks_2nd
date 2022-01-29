2b4cdcb5f880e710780bc6c3bc49f0cca08d3bbe
Tom-chack <tom.controlcenter@gmail.com>
2022-01-29 11:32:03 +0400

    Delete message

--- messages-redux/src/components/Message.jsx
+++ messages-redux/src/components/Message.jsx
@@ -1,19 +1,22 @@
 import React, {useState} from 'react';
-import {useSelector} from "react-redux";
+import {useSelector, useDispatch} from "react-redux";
 import Editor from './Editor';
+import {removeMessage} from '../redux/ducks/messageDuck'
 
 function Message({message}) {
 
+  const dispatch = useDispatch();
   const user = useSelector( state => state.userDuck.user );
 
   const [edit, setEdit] = useState(false);
-
   const handleSave = () => setEdit(false);
   const handleCancel = () => setEdit(false);
 
+  const deleteMessage = "Are you sure you want to delete this message?";
+
   const buttons = <div className="actions">
                     <button className="btn btn-outline-primary btn-sm" onClick={() => setEdit(true)}>Edit</button>
-                    <button className="btn btn-outline-danger btn-sm">Delete</button>
+                    <button className="btn btn-outline-danger btn-sm" onClick={() => { window.confirm(deleteMessage) && dispatch(removeMessage(message)) } }>Delete</button>
                   </div>
 
   return ( 
