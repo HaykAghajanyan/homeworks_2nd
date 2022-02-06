77197f0b4d25e12a5bc1e26d30a9b2763f334498
Hayk Aghajanyan <aghajanyan.haykv@gmail.com>
2022-02-06 17:38:32 +0400

    lazy loading

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import Header from "./components/Header";
 import {Route, Routes, useNavigate} from "react-router-dom";
-import Messages from "./components/Messages";
-import Configs from "./components/Configs";
-import {useCallback, useEffect, useState} from "react";
-import PopupWrapper from "./components/PopupWrapper";
+// import Messages from "./components/Messages";
+// import Configs from "./components/Configs";
+import {useCallback, useEffect, useState, lazy, Suspense} from "react";
+// import PopupWrapper from "./components/PopupWrapper";
 import PopupMessage from "./components/PopupMessage";
 import {useDispatch} from "react-redux";
 import {fetchMessages} from "./redux/ducks/messageDuck";
-import AuthComponent from "./components/AuthComponent";
+// import AuthComponent from "./components/AuthComponent";
 import {fetchId} from "./redux/ducks/appDuck";
+// import Cats from "./components/Cats";
+
+const Messages = lazy(() => import('./components/Messages'))
+const Configs = lazy(() => import('./components/Configs'))
+const PopupWrapper = lazy(() => import('./components/PopupWrapper'))
+const AuthComponent = lazy(() => import('./components/AuthComponent'))
+const Cats = lazy(() => import('./components/Cats'))
 
 const App = () => {
     const [configs, setConfigs] = useState({})
@@ -34,15 +41,18 @@ const App = () => {
     return (
         <>
             <Header/>
-            <Routes>
-                <Route path='' element={<Messages configs={configs}/>}/>
-                <Route path='configs' element={<Configs handleConfigs={handleConfigs}/>}/>
-                <Route path='auth' element={<AuthComponent/>} />
-                <Route path=':id' element={
-                    <PopupWrapper onClose={goBack}>
-                        <PopupMessage/>
-                    </PopupWrapper>}/>
-            </Routes>
+            <Suspense fallback={<div>...Loading</div>}>
+                <Routes>
+                    <Route path='' element={<Messages configs={configs}/>}/>
+                    <Route path='configs' element={<Configs handleConfigs={handleConfigs}/>}/>
+                    <Route path='auth' element={<AuthComponent/>} />
+                    <Route path='cats' element={<Cats/>} />
+                    <Route path=':id' element={
+                        <PopupWrapper onClose={goBack}>
+                            <PopupMessage/>
+                        </PopupWrapper>}/>
+                </Routes>
+            </Suspense>
         </>
     );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -20,6 +20,7 @@ const Header = () => {
         <header className='header'>
             <NavLink onClick={() => handleRouteChange('')} className='navLink' to=''>to Messages</NavLink>
             <NavLink onClick={() => handleRouteChange('configs')} className='navLink' to='configs'>to Configs</NavLink>
+            <NavLink onClick={() => handleRouteChange('cats')} className='navLink' to='cats'>to Cats</NavLink>
             {
                 currentUser ?
                     <button className='log-out' onClick={logOut}>Log out</button>
